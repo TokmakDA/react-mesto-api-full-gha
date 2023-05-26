0fdfd301577d42ca79be259003407d73b111bba2
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-05-26 17:08:13 +0300

    Token

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -24,6 +24,7 @@ module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
+  console.log('corse = { origin } = req.headers', origin);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -20,29 +20,29 @@ function checkToken(token) {
     return false;
   }
   try {
-    // // Проверяем по ключу DEV
-    const payload = jwt.verify(token, SECRET_KEY_DEV);
-    console.log(
-      '\x1b[31m%s\x1b[0m',
-      `Надо исправить. В продакшне используется тот же
-          секретный ключ, что и в режиме разработки.`,
-    );
+    // // // Проверяем по ключу DEV
+    // const payload = jwt.verify(token, SECRET_KEY_DEV);
+    // console.log(
+    //   '\x1b[31m%s\x1b[0m',
+    //   `Надо исправить. В продакшне используется тот же
+    //       секретный ключ, что и в режиме разработки.`,
+    // );
 
     // Проверяем по ключу Production и возвращаем пейлоуд
     return jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    // Доп проверка если ошибка по ключу DEV
-    if (
-      err.name === 'JsonWebTokenError' &&
-      err.message === 'invalid signature'
-    ) {
-      console.log(
-        '\x1b[32m%s\x1b[0m',
-        'Всё в порядке. Секретные ключи отличаются',
-      );
-    } else {
-      console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
-    }
+    // // Доп проверка если ошибка по ключу DEV
+    // if (
+    //   err.name === 'JsonWebTokenError' &&
+    //   err.message === 'invalid signature'
+    // ) {
+    //   console.log(
+    //     '\x1b[32m%s\x1b[0m',
+    //     'Всё в порядке. Секретные ключи отличаются',
+    //   );
+    // } else {
+    //   console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
+    // }
     return false;
   }
 }

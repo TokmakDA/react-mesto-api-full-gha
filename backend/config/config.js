b8457bfc629877b0bf1537ad6fa5da2f4447f8af
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-05-28 21:33:52 +0300

    back full

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ const COOKIE_PARAMS = {
   maxAge: 3600000 * 24 * 7,
   httpOnly: true,
   sameSite: 'None',
-  secure: true, // с этим параметром тесты валятся и постман не работает cookie работают только по https
+  secure: true,
 };
 
 module.exports = {

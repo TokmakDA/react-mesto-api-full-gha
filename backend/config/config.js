b301b2cfcaa2caf53c598236ef398fd39ff241ed
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-05-29 17:40:35 +0300

    secure for Site

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,8 @@
 const COOKIE_PARAMS = {
   maxAge: 3600000 * 24 * 7,
-  httpOnly: false,
-  // sameSite: 'None',
-  // secure: true,
+  httpOnly: true,
+  sameSite: 'None',
+  secure: true,
 };
 
 module.exports = {

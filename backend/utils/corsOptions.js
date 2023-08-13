d74d7a92d9806d6e405e623e7cb62602cd8073ea
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-08-13 14:08:40 +0300

    fix cors

--- backend/utils/corsOptions.js
+++ backend/utils/corsOptions.js
@@ -4,15 +4,12 @@ const allowedCors = [
   'localhost:3000',
   'http://localhost:3000',
 
-  'http://e-tokmak.ru',
-  'https://e-tokmak.ru',
   'http://api.mesto.e-tokmak.ru',
   'https://api.mesto.e-tokmak.ru',
   'http://mesto.e-tokmak.ru',
   'https://mesto.e-tokmak.ru',
   'api.mesto.e-tokmak.ru',
   'mesto.e-tokmak.ru',
-  'e-tokmak.ru',
 ];
 // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
